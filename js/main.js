67af0b7ae0658200579f944bba65f153b29bbeb1
arielle_b <ariellebosshard@outlook.com>
2022-01-28 17:07:24 +1100

    first init

---
+++ js/main.js
@@ -0,0 +1,29 @@
+//MOBILE HERO SLIDESHOW
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 3500); // Change image every 4 seconds
+}
+
+//PROJECT GALLERY SLIDESHOW 
+
+const buttonRight = document.getElementById('slideRight');
+
+buttonRight.onclick = function () {
+  document.getElementById('container').scrollLeft += 20;
+};
+
+const buttonLeft = document.getElementById('slideLeft');
+
+buttonLeft.onclick = function () {
+  document.getElementById('container').scrollLeft -= 20;
+};
